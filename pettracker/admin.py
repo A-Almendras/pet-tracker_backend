fa6c4fcadefdec60f69d48150ece1cc667ab7435
Anaflavia Almendras <anaflaviaalmendras@gmail.com>
2022-09-06 00:57:39 -0400

    wip: register models for full CRUD in admin dash

--- pettracker/admin.py
+++ pettracker/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Pet, Expense, Observation, Record
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Pet)
+admin.site.register(Expense)
+admin.site.register(Observation)
+admin.site.register(Record)
