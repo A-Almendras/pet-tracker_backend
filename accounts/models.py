55e50e2b9f58550835026f4031875b34d7250e66
Anaflavia Almendras <anaflaviaalmendras@gmail.com>
2022-09-07 15:00:51 -0400

    wip: working on custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class NewUser(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    location = models.CharField(max_length=100, blank=True)
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=500)
+    # user_image =
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+
+class CustomUserManager(BaseUserManager):
+    pass
--- pettracker_django/settings.py
+++ pettracker_django/settings.py
@@ -55,6 +55,7 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        # 'rest_framework.permissions.IsAuthenticatedOrReadOnly'
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
