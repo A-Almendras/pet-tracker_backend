973f31b9536c7b3df4728c742ab79c28c4e5f6ef
Anaflavia Almendras <anaflaviaalmendras@gmail.com>
2022-09-09 04:10:35 -0400

    wip: fixed url bug for register

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import CreateNewUserView
 
 urlpatterns = [
-    path('register/', CreateNewUserView.as_view(), name='create_user')
+    path('register', CreateNewUserView.as_view(), name='create_user')
 ]
