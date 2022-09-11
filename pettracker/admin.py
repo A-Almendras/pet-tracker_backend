2bfa0e4b92c27ebc4c4ed3348f69d6830bc1fe2d
Anaflavia Almendras <anaflaviaalmendras@gmail.com>
2022-09-10 22:56:53 -0400

    wip: configured pet admin dash

--- pettracker/admin.py
+++ pettracker/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
-from .models import User, Pet, Expense, Observation, Record
+from .models import Pet, Expense, Observation, Record
+
+
+class PetAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'user_id'
+    )
+
 
 # Register your models here.
-# admin.site.register(User)
-admin.site.register(Pet)
+# admin.site.register(UserAdmin)
+admin.site.register(Pet, PetAdmin)
 admin.site.register(Expense)
 admin.site.register(Observation)
 admin.site.register(Record)
