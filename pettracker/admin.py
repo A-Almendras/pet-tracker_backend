f57e5a99d32d817e2283031daa6253ab19417e7b
Anaflavia Almendras <anaflaviaalmendras@gmail.com>
2022-09-15 16:52:27 -0400

    wip: modified pet admin panel

--- pettracker/admin.py
+++ pettracker/admin.py
@@ -1,3 +1,5 @@
+from pyexpat import model
+from unicodedata import name
 from django.contrib import admin
 from .models import Pet, Expense, Observation, Record
 
@@ -8,6 +10,14 @@ class PetAdmin(admin.ModelAdmin):
         'name',
         'user_id'
     )
+    list_display_links = ['id', 'user_id']
+    list_editable = ['name']
+    # list_filter = []
+
+    search_fields = ['user_id']
+
+    class Meta:
+        model = Pet
 
 
 # Register your models here.
